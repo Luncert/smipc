aa561f1d19e12d12b2dfb75cf31ba452de507151
Luncert <2725115515@qq.com>
2019-11-26 16:06:53 +0800

    Add setup.py.

---
+++ py/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='smipc',
+    version='1.0.0',
+    py_modules=['smipc'],
+    author='Luncert',
+    author_email='2725115515@qq.com',
+    url='https://github.com/Luncert/smipc',
+    description='IPC lib based on shared memory.'
+)
