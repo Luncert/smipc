4c31629162b06fd7a4d8cdce66a4e46c69481d87
Luncert <2725115515@qq.com>
2019-11-26 14:14:40 +0800

    Fix bug

--- nodejs/index.js
+++ nodejs/index.js
@@ -3,4 +3,4 @@ const path = require('path')
 const lib = require('./build/Release/smipc.node')
 lib.loadSmipc(path.join(__dirname, 'lib/libsmipc.dll'))
 delete lib['loadSmipc']
-exports = lib
+module.exports = lib
